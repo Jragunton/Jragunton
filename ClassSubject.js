c2e123a8dd5906b017b92a9df35d7d72799926df
Jragunton <163392476+Jragunton@users.noreply.github.com>
2024-10-31 22:48:20 +0800

    Create ClassSubject.js

---
+++ ClassSubject.js
@@ -0,0 +1,9 @@
+// Collecting information
+const subjectTitle = "Data Structures and Algorithm"; // Example subject title
+const classSchedule = " 10:30-11:30 MT"; // Example class schedule
+const classroom = "Cor B"; // Example classroom
+const classInstructor = " Cliff Owen Pascua"; // Example instructor name
+const studentName = "Joeben Ragunton"; // Example student name
+
+// Logging the information to the console
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
