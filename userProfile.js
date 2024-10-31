685b47214c8764b2abc2302e955167af44214e77
Jragunton <163392476+Jragunton@users.noreply.github.com>
2024-10-31 22:26:08 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+// Collect user information
+let fullName = "Joeben Ragunton";
+let age = 21;
+let favoriteNumber = 7;
+let favoriteColor = "blue";
+
+// Log the information to the console
+console.log(fullName, age, favoriteNumber, favoriteColor);
+
