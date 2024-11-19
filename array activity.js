295120b3b3cff456a3e1fba8eab2da91f8a52dc9
Jragunton <163392476+Jragunton@users.noreply.github.com>
2024-11-19 15:06:47 +0800

    arrayActivity.js

---
+++ array activity.js
@@ -0,0 +1,16 @@
+// Create an array called favSingers with three of my favorite singers
+let favSingers = ['Jhong Madaliday','Bruno Mars', 'Justine Bieber'];
+
+// Log the first singer from the favSingers array
+console.log(favSingers[0]); // Output:Jhong Madaliday(because Jhong Madaliday is at index 0)
+
+// Create an array called favNumbers with four of my favorite numbers
+let FavNumbers= [7, 2, 9, 8];
+
+// Create an array called mixedArr with different types of data
+let mixedArr = ['Joeben', 123, true];
+
+// Log each item from the mixedArr array using its index
+console.log(mixedArr[0]); // Output: Joeben (this is the first item)
+console.log(mixedArr[1]); // Output: 123 (this is the third item, a number)
+console.log(mixedArr[2]); // Output: true (this is the fourth item, a boolean)
